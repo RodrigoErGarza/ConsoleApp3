8bd8a505f8b2f25f834fa78424085abee2e0403f
Walter Garcia <Walter Garcia@DESKTOP-ARKTHNQ>
2022-02-18 23:02:23 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("HOLA!!");
+Console.WriteLine("Por favor ingresa tu edad: ");
+byte edad= byte.Parse(Console.ReadLine());
+Console.WriteLine("Enserio!? No pareces que tengas" + edad + " años ");
+Console.ReadKey();
